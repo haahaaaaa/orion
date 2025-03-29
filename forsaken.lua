27d442f17cf74600db61919713449e51f5f732ce
haahaaaaa <85419633+haahaaaaa@users.noreply.github.com>
2025-03-28 15:32:32 -1000

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -44,7 +44,7 @@ local function esp()
         billboard.AlwaysOnTop = true
         billboard.Size = UDim2.new(5, 0, 3, 0)
         billboard.StudsOffset = Vector3.new(0, -2, 0)
-        billboard.Parent = v
+        billboard.Parent = v:FindFirstChild("HumanoidRootPart")
 
         local text = Instance.new("TextLabel")
         text.Text = v.Name
@@ -61,7 +61,7 @@ local function esp()
         hpb.AlwaysOnTop = true
         hpb.Size = UDim2.new(5, 0, 3, 0)
         hpb.StudsOffset = Vector3.new(0, -3.5, 0)
-        hpb.Parent = v
+        hpb.Parent = v:FindFirstChild("HumanoidRootPart")
 
         local hpt = Instance.new("TextLabel")
         hpt.Text = tostring(math.round(v:WaitForChild("Humanoid").Health)).. "/".. tostring(v:FindFirstChild("Humanoid").MaxHealth)
@@ -100,7 +100,7 @@ local function esp()
         billboard.AlwaysOnTop = true
         billboard.Size = UDim2.new(5, 0, 3, 0)
         billboard.StudsOffset = Vector3.new(0, -2, 0)
-        billboard.Parent = v
+        billboard.Parent = v:FindFirstChild("HumanoidRootPart")
 
         local text = Instance.new("TextLabel")
         text.Text = v.Name
@@ -117,7 +117,7 @@ local function esp()
         hpb.AlwaysOnTop = true
         hpb.Size = UDim2.new(5, 0, 3, 0)
         hpb.StudsOffset = Vector3.new(0, -3.5, 0)
-        hpb.Parent = v
+        hpb.Parent = v:FindFirstChild("HumanoidRootPart")
 
         local hpt = Instance.new("TextLabel")
         hpt.Text = tostring(math.round(v:WaitForChild("Humanoid").Health)).. "/".. tostring(v:FindFirstChild("Humanoid").MaxHealth)
